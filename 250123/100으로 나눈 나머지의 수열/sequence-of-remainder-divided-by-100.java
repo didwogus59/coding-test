01a52757a7eb067d423efbf22963437b492c675d
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-01-23 15:18:08 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 126ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250123/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        arr = new int[n + 1];
+        arr[1] = 2;
+        arr[2] = 4;
+        System.out.println(fact(n));
+    }
+    public static int fact(int n) {
+        if(arr[n] == 0)
+            arr[n] = (fact(n - 1) * fact(n - 2)) % 100;
+        return arr[n];
+    }
+}
