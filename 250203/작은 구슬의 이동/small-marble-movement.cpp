06cab23391791d70265b0025403681eaa99c5c3e
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-03 17:52:16 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250203/작은 구슬의 이동/small-marble-movement.cpp
+++ 250203/작은 구슬의 이동/small-marble-movement.cpp
@@ -14,33 +14,28 @@ int main() {
     int dirX[] = {-1,0,0,1};
     int dirY[] = {0,-1,1,0};
 
-    int dx,dy;
+    int dir;
 
     if(d == 'U') {
-        dx = 4;
-        dy = 4;
+        dir = 3;
     }
     else if(d == 'D') {
-        dx = 0;
-        dy = 0;
+        dir = 0;
     }
     else if(d == 'R') {
-        dx = 2;
-        dy = 2;
+        dir = 2;
     }
     else {
-        dx = 1;
-        dy = 1;
+        dir = 1;
     }
     
     for(int i = 0; i < t; i++) {
-        x += dirX[dx];
-        y += dirY[dy];
+        x += dirX[dir];
+        y += dirY[dir];
         if(!range(x,y)) {
-            x -= dirX[dx];
-            y -= dirY[dy];
-            dx = 3 - dx;
-            dy = 3 - dy;
+            x -= dirX[dir];
+            y -= dirY[dir];
+            dir = 3 - dir;
         }
     }
     cout << x << " " << y;
