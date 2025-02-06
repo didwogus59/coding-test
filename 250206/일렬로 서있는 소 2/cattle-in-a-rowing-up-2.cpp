cb245eb3b4944110862892003cac5ecc98e03f0f
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-06 16:27:49 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250206/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
+++ 250206/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -13,9 +13,9 @@ int main() {
     int ans = 0;
     for(int i1 = 0; i1 < N; i1++) {
         for(int i2 = i1 + 1; i2 < N; i2++) {
-            if(A[i1] < A[i2]) {
+            if(A[i1] <= A[i2]) {
                 for(int i3 = i2 + 1; i3 < N; i3++) {
-                    if(A[i2] < A[i3])
+                    if(A[i2] <= A[i3])
                         ans++;
                 }
             }
