c69bd8634319f0e53d67739ee133379cfac5134c
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-07 13:48:42 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250207/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[20];
+bool no_carry(int a, int b, int c);
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int ans = 0;
+    for(int i = 0; i < n - 2; i++) {
+        for(int j = i + 1; j < n - 1; j++) {
+            for(int p = j + 1; p < n; p++) {
+                int sum = arr[i] + arr[j] + arr[p];
+                if(no_carry(arr[i], arr[j], arr[p])) {
+                    ans = sum > ans ? sum : ans;
+                }
+            } 
+        }
+    }
+    cout << ans;
+    // Write your code here!
+
+    return 0;
+}
+bool no_carry(int a, int b, int c) {
+    while(a + b + c > 0) {
+        if(a % 10 + b % 10 + c % 10 >= 10)
+            return false;
+        a /= 10;
+        b /= 10;
+        c /= 10;
+    }
+    return true;
+}
