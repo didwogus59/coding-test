47c85c2f805e7ac8fc968db0069d17a5ad055eed
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-07 14:03:34 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250207/오목/O-mok.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int board[19][19];
+bool range(int x, int y) {
+    if(x >= 0 && x < 19 && y >= 0 && y < 19)
+        return true;
+    return false;
+}
+int dirX[3] = {1,0,1};
+int dirY[3] = {0,1,1};
+int main() {
+    for (int i = 0; i < 19; i++)
+        for (int j = 0; j < 19; j++) cin >> board[i][j];
+
+    for(int i = 0; i < 19; i++) {
+        for(int j = 0; j < 19; j++) {
+            if(board[i][j] != 0) {
+                for(int p = 0; p < 3; p++) {
+                    bool chk = true;
+                    for(int q = 1; q < 5; q++) {
+                        int x = i + dirX[p] * q;
+                        int y = j + dirY[p] * q;
+                        if(range(x, y)) {
+                            if(board[x][y] != board[i][j])
+                                chk = false;
+                        }
+                        else
+                            chk = false;
+                    }
+                    if(chk) {
+                        cout << board[i][j] << '\n' << i + dirX[p] * 2 + 1 << " " << j + dirY[p] * 2 + 1;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
