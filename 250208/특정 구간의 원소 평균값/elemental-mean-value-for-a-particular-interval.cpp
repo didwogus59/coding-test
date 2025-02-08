c717046f65b209680e69dee4af6eb3e0bdd6c988
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-08 23:23:16 +0900

    Title: 특정 구간의 원소 평균값, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250208/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int ans = n;
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            int sum = 0;
+            for(int p = i; p <= j; p++) {
+                sum += arr[p];
+            }
+            int mean = sum / (j - i);
+            if(mean * (j - i) == sum) {
+                for(int p = i; p <= j; p++) {
+                    if(mean == arr[p]) {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
