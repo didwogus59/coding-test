a67f267f2ad914632cd1512c0a6b98a8607ff1c3
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-12 13:26:54 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250212/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 250212/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -11,16 +11,25 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
     }
-    int ans =n;
+    int* cnt = new int[n];
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(i != j) {
                 if(x1[i] < x1[j]) {
-                    if(x2[i] > x2[j])
-                        ans -= 2;
+                    if(x2[i] > x2[j]) {
+                        cnt[i] = 1;
+                        cnt[j] = 1;
+                    }
                 }
+
             }
+
         }
+    }
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        if(cnt[i] == 0)
+            ans++;
     }
     cout << ans;
     return 0;
