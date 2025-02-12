eb3c92b066b5e8fae2caf3836e1988c2ed3f5a4a
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-12 14:35:09 +0900

    Title: 상해버린 치즈, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250212/상해버린 치즈/rotten-cheese.cpp
+++ 250212/상해버린 치즈/rotten-cheese.cpp
@@ -33,6 +33,8 @@ int main() {
                     break;
                 }
             }
+            if(!chk)
+                break;
         }
         if(chk) {
             int* cnt = new int[N];
