e1b226d9076445b69433e1c8550f0ff7f4a9604a
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-13 13:47:28 +0900

    Title: 야바위, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250213/야바위/ya-rock.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int a[100], b[100], c[100];
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> a[i] >> b[i] >> c[i];
+    }
+
+    // Write your code here!
+    
+    int ans = 0;
+    int max = 0;
+    for(int i = 1; i < 4; i++) {
+        int cnt = 0;
+        int* coin = new int[3]{};
+        coin[i] = 1;
+        for(int j = 0; j < N; j++) {
+            int tmp = coin[a[j]];
+            coin[a[j]] = coin[b[j]];
+            coin[b[j]] = tmp;
+            cnt += coin[c[j]];
+        }
+        if(max < cnt) {
+            ans = i;
+            max = cnt;
+        }
+    }
+    cout << ans;
+    return 0;
+}
