da82e38913afaed563cf7b4e5f12013c1c28d0e8
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-13 17:11:28 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250213/3개의 선 2/three-lines-2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x[20], y[20];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+    for(int i = 0; i < 20; i++) {
+        int* arr = new int[n]{};
+        for(int j = 0; j < 20; j++) {
+            for(int p = 0; p < 20; p++) {
+                for(int q = 0; q < n; q++) {
+                    if(i < 11) {
+                        if(x[q] == i) {
+                            arr[q] = 1;
+                        }
+                    }
+                    if(i > 10) {
+                        if(y[q] == i) {
+                            arr[q] - 1;
+                        }
+                    }
+                    if(j < 11) {
+                        if(x[q] == j) {
+                            arr[q] = 1;
+                        }
+                    }
+                    if(j > 10) {
+                        if(y[q] == j) {
+                            arr[q] - 1;
+                        }
+                    }
+                    if(p < 11) {
+                        if(x[q] == p) {
+                            arr[q] = 1;
+                        }
+                    }
+                    if(p > 10) {
+                        if(y[q] == p) {
+                            arr[q] - 1;
+                        }
+                    }
+                }
+                bool chk = true;
+                for(int q = 0; q < n; q++) {
+                    if(arr[q] == 0) {
+                        chk = false;
+                        break;
+                    }
+                }
+                if(chk) {
+                    cout << 1;
+                    return 0;
+                }
+            }
+        }
+    }
+    // Write your code here!
+    cout << 0;
+    return 0;
+}
