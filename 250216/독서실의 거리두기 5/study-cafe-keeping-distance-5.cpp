bd11d25a41825d82ca1e918014c73ae637c4960b
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-16 14:56:41 +0900

    Title: 독서실의 거리두기 5, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250216/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+string seat;
+
+int main() {
+    cin >> N;
+    cin >> seat;
+    int len = seat.length();
+    int* arr = new int[len];
+    for(int i = 0; i < len; i++) {
+        if(seat[i] == '0') {
+            arr[i] = 0;
+        }
+        else
+            arr[i] = 1;
+    }
+    int ans = 0;
+    for(int i = 0; i < len; i++) {
+        if(arr[i] == 0) {
+            arr[i] = 1;
+            int dis = len + 1;
+            for(int j = 1; j < len; j++) {
+                if(i + j < len && arr[i + j] == 1) {
+                    dis = dis < j ? dis : j;
+                }
+                if(i - j >= 0 && arr[i - j] == 1) {
+                    dis = dis < j ? dis : j;
+                }
+            }
+            ans = ans > dis ? ans : dis;
+            arr[i] = 0;
+        }
+    }
+    cout << ans;
+    return 0;
+}
