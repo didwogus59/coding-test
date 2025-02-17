7bbce3ad17b1004d81f3f85d3ca4fd0a55fc00a9
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-17 10:37:31 +0900

    Title: 훌륭한 점프, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250217/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int ans = 200;
+int n, k;
+int* arr;
+void jump(int max, int now) {
+    if(now != n) {
+        for(int i = now; i < n && i - now < k; i++) {
+            if(arr[i] > max) {
+                jump(arr[i], i + 1);
+            }
+            else {
+                jump(max, i + 1);
+            }
+        }
+    }
+    else {
+        ans = ans < max ? ans : max;
+    }
+}
+int main() {
+    cin >> n >> k;
+
+    arr = new int[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    jump(0, 0);
+
+    cout << ans;
+    return 0;
+}
