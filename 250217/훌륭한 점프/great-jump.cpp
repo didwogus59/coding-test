36e9153ab6356995ab59c12da8cd621c0b28b346
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-17 15:27:33 +0900

    Title: 훌륭한 점프, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250217/훌륭한 점프/great-jump.cpp
+++ 250217/훌륭한 점프/great-jump.cpp
@@ -5,20 +5,20 @@ using namespace std;
 int n, k;
 int arr[100];
 bool chk = false;
-void jump(int now, int min) {
-    if(now == min) {
-        chk = true;
-    }
-    else {
-        int max = -1;
+bool jump(int min) {
+    int now = 0;
+    while(now != n - 1) {
+        int max = 0;
         for(int i = 1; i <= k && i + now < n; i++) {
             if(arr[now + i] <= min) {
                 max = now + i;
             }
         }
-        if(max != - 1)
-            jump(max, min);
+        if(max == 0)
+            return false;
+        now = max;
     }
+    return true;
 }
 int main() {
     cin >> n >> k;
@@ -28,11 +28,11 @@ int main() {
 
     for(int i = 1; i <= 100; i++) {
         int max = i > arr[0] ? i : arr[0];
-        jump(0,max);
-        if(chk) {
+        if(jump(max)) {
             cout << max;
             return 0;
         }
+        
     }
     // Write your code here!
 
