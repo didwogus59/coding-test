1132f9a0751c1bff52314fb43226d691ee04f2fd
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-18 14:42:14 +0900

    Title: 구간 잘 나누기, Time: 1093ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 250218/구간 잘 나누기/divide-sections-well.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+
+int n, m;
+int* arr;
+int ans = 101;
+void div(int now, int cnt, int max) {
+    if(now < n && cnt < m - 1) {
+        int sum = 0;
+        for(int i = now; i < n; i++) {
+            sum += arr[i];
+            max = max > sum ? max : sum;
+            div(i + 1, cnt + 1, max);
+        }
+    }
+    else {
+        if(cnt >= m - 1) {
+            
+            int sum = 0;
+            for(int i = now; i < n; i++) {
+                sum += arr[i];
+            }
+            max = max > sum ? max : sum;
+        }
+        ans = ans < max ? ans : max;
+    }
+}
+int main() {
+    cin >> n >> m;
+    arr = new int[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    div(0,0,0);
+    cout << ans;
+    return 0;
+}
