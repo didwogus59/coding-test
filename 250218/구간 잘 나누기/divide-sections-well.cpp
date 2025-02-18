8c3c41f97fe6d5f7c4378f8482c7a790333dcef0
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-18 14:57:33 +0900

    Title: 구간 잘 나누기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250218/구간 잘 나누기/divide-sections-well.cpp
+++ 250218/구간 잘 나누기/divide-sections-well.cpp
@@ -4,18 +4,15 @@ using namespace std;
 int n, m;
 int* arr;
 int ans = 101;
-int chk[101][101]; 
 void div(int now, int cnt, int max) {
     if(now < n && cnt < m - 1) {
         int sum = 0;
         for(int i = now; i < n; i++) {
             sum += arr[i];
+            if(sum >= ans)
+                break;
             max = max > sum ? max : sum;
-            if(chk[i][cnt] == 0) {
-                chk[i][cnt] = 1;
-                div(i + 1, cnt + 1, max);
-
-            }
+            div(i + 1, cnt + 1, max);
         }
     }
     else {
