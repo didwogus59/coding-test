de63027ca6fd4624090706c6928c3fb2ab62d5ac
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-19 15:00:12 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250219/코딩톡/coding-talk.cpp
+++ 250219/코딩톡/coding-talk.cpp
@@ -13,15 +13,21 @@ int main() {
         cin >> c[i] >> u[i];
     }
 
-    int user[26] = {};
+    int user[27] = {};
 
     for(int i = p -1; i < m; i++) {
         user[int(c[i] - 'A')] = 1;
     }
-
-    for(int i = 0; i < n; i++) {
-        if(user[i] == 0)
-            cout << char(i + 'A') << " ";
+    // for(int i = 0; i < n; i++) {
+    //     if(user[i] == 1) {
+    //         cnt++;
+    //     }
+    // }
+    if(u[p-1] != 0) {
+        for(int i = 0; i < n; i++) {
+            if(user[i] == 0)
+                cout << char(i + 'A') << " ";
+        }
     }
 
     return 0;
