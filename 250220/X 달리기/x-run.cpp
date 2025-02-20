66a1504f0e70a8db097d4619619d946b73c8a9b9
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-20 10:35:28 +0900

    Title: X 달리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250220/X 달리기/x-run.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = n*n;
+    int down[1000];
+    for(int i = 1; i < 5; i++) {
+        int cnt = 0;
+        int spd = 1;
+        int dis = 0;
+        while(spd <= i) {
+            dis += spd;
+            down[spd + 1] = dis;
+            spd++;
+            cnt++;
+        }
+        spd--;
+        while(spd > 1) {
+            // cout << i << " " << dis << " " << spd << " " << down[spd] << '\n';
+            if(dis + down[spd] + spd > n) {
+                spd--;
+            }
+            dis += spd;
+            cnt++;
+        }
+        if(dis < n) {
+            cnt += n - dis;
+            dis = n;
+        }
+
+        if(dis == n) {
+            ans = ans < cnt ? ans : cnt;
+        }
+        else if(dis < n) {
+            cnt += n - dis;
+            ans = ans < cnt ? ans : cnt;
+        }
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
