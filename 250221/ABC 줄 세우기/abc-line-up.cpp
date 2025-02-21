cf1a92bb1772510faa794703b3c783b7a5a4fe20
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2025-02-21 09:14:33 +0900

    Title: ABC 줄 세우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250221/ABC 줄 세우기/abc-line-up.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arrP[26];
+
+int arrM[26];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        char tmp;
+        cin >> tmp;
+        arrP[i] = (int)(tmp - 'A');
+        
+        arrM[i] = (int)(tmp - 'A');
+    }
+    // Write your code here!
+    int ans;
+    int cnt = 0;
+    for(int i = n - 1; i >= 0; i--) {
+        int max = -1;
+        int p = -1;
+        for(int j = 0; j <= i; j++) {
+            if(arrP[j] > max) {
+                max = arrP[j];
+                p = j;
+            }
+        }
+        for(int j = p; j < i; j++) {
+            int tmp = arrP[j];
+            arrP[j] = arrP[j+1];
+            arrP[j+1] = tmp;
+        }
+        cnt += i - p;
+    }
+    ans = cnt;
+    cnt = 0;
+    for(int i = n - 1; i >= 0; i--) {
+        int min = 10000;
+        int p = -1;
+        for(int j = 0; j <= i; j++) {
+            if(arrM[j] < min) {
+                min = arrM[j];
+                p = j;
+            }
+        }
+        for(int j = p; j < i; j++) {
+            int tmp = arrM[j];
+            arrM[j] = arrM[j+1];
+            arrM[j+1] = tmp;
+        }
+        cnt += i - p;
+    }
+    ans = ans < cnt ? ans : cnt;
+    cout << ans;
+    return 0;
+}
