c29c7c6149ce020f7589d767b26bc188f6219014
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-04-07 23:06:13 +0900

    Title: 행복한 수열의 개수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260407/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, m;
+    cin >> n >> m;
+    int** arr = new int*[n];
+    for(int i = 0; i < n; i++) {
+        arr[i] = new int[n];
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j]; 
+        }
+    }
+    int sum = 0;
+    for(int i = 0 ; i < n; i++) {
+        int cntR = 1;
+        int cntC = 1;
+        int chkR = 0;
+        int chkC = 0;
+        for(int j = 1; j < n; j++) {
+            if(arr[i][j] == arr[i][j - 1]) {
+                cntR += 1;
+            }
+            else {
+                cntR = 1;
+            }
+            if(arr[j][i] == arr[j - 1][i]) {
+                cntC += 1;
+            }
+            else {
+                cntC = 1;
+            }
+
+            if(cntR >= m) {
+                chkR = 1;
+            }
+            if(cntC >= m) {
+                chkC = 1;
+            }
+        }
+        sum = sum + chkR + chkC;
+    }
+    cout << sum;
+    return 0;
+}
