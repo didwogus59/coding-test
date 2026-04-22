401af4bb9c720bf4a004e3cd61b99b266c8053ca
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-04-22 15:22:51 +0900

    Title: 양수 직사각형의 최대 크기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260422/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 260422/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int cal(int x1, int x2, int y1, int y2, int** arr) {
     for(int x = x1; x <= x2; x++) {
         for(int y = y1; y <= y2; y++) {
-            if(arr[x][y] < 0)
+            if(arr[x][y] <= 0)
                 return -1;
         }
     }
