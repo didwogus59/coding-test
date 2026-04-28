ca0e71516a61f94105a6a58b5d32aa9438c8ebf6
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-04-28 14:04:35 +0900

    Title: 최단 Run Length 인코딩, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260428/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Main {
+    static int[] mx = {0, 1, 0, -1};
+    static int[] my = {1, 0, -1, 0};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int len = str.length();
+        int[] arr = new int[len];
+        for(int i = 0; i < len; i++) {
+            arr[i] = str.charAt(i);
+        }
+        int ans = len;
+        for(int i = 0; i < len; i++) {
+            int cnt = 1;
+            int sum = 0;
+            int bef = arr[i];
+            for(int j = 1; j < len; j++) {
+                int now = (i + j) % len;
+                if(arr[now] == bef) {
+                    cnt++;
+                }
+                else {
+                    sum = sum + 1 + digit(cnt);
+                }
+                bef = arr[now];
+            }
+            sum = sum + 1 + digit(cnt);
+            if(sum < ans)
+                ans = sum;
+        }
+        System.out.println(ans);
+
+    }
+    static int digit(int num) {
+        int cnt = 1;
+        while(num >= 10) {
+            cnt++;
+            num /= 10;
+        }
+        return cnt;
+    }
+}
