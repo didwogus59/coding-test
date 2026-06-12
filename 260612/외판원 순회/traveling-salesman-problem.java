a6c7e5cf15be48e0648f976057f686c032f74516
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-06-12 14:12:36 +0900

    [Passed] 외판원 순회 (180ms, 11MB)

---
+++ 260612/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    static int ans = Integer.MAX_VALUE;
+    static int len;
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        len = sc.nextInt();
+
+        int[] visited = new int[len];
+        int[][] arr = new int[len][len];
+
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        visited[0] = 1;
+        bt(arr, visited, 0, 0, 1);
+
+        System.out.println(ans);
+    }
+    static void bt(int[][] arr, int[] visited, int now, int bef,int cnt) {
+            if (cnt == len) {
+                if(arr[bef][0] != 0)
+                    ans = Math.min(now + arr[bef][0], ans);
+            }
+            else {
+                for(int i = 0; i < len; i++) {
+                    if(visited[i] == 0 && arr[bef][i] != 0) {
+                        visited[i] = 1;
+                        bt(arr, visited, now + arr[bef][i], i,cnt + 1);
+                        visited[i] = 0;
+                    }
+                }
+            }
+    }
+
+    static boolean inRange(int x, int y) {
+        return x >= 0 && y >= 0 && x < len && y < len;
+    }
+    static int dis(pair p1, pair p2) {
+        int dx = p1.x - p2.x;
+        int dy = p1.y - p2.y;
+        return dx * dx + dy * dy;
+    }
+    static int dis(int x, int y, int nx, int ny) {
+        int dx = nx - x;
+        int dy = ny - y;
+        dx = dx > 0 ? dx : (dx * -1);
+        dy = dy > 0 ? dy : (dy * -1);
+        return dx + dy;
+    }
+
+    static class pair {
+        public int x;
+        public int y;
+        public pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
+
