b996c450c2249d747c1801ea47a67996c878ba7d
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-07-29 14:42:09 +0900

    [Passed] 사각형 채우기 2 (133ms, 10MB)

---
+++ 260729/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    static int ans = 0;
+    static int len;
+    static int m;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static long[] memo;
+    static long[] memo2;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        memo = new long[2000];
+        memo2 = new long[2000];
+
+        memo[0] = 1;
+        memo[1] = 1;
+        memo[2] = 3;
+        dp(n);
+        System.out.println(memo[n]);
+    }
+    static long dp(int n) {
+        if(memo[n] == 0) {
+            memo[n] = (dp(n - 1) + dp(n - 2) * 2) % 10007;
+        }
+        return memo[n];
+    }
+    //
+    static boolean inRange(int x, int y, int lenX, int lenY) {
+        return x >= 0 && y >= 0 && x < lenX && y < lenY;
+    }
+    static class pair {
+        int x;
+        int y;
+        int cnt = 0;
+        int now = 0;
+        public pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public pair(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+}
+
+
