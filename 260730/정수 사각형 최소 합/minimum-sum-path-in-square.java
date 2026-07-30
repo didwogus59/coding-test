b2bae1fe3be9007ab927ac7e5ce434fa30c694c4
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-07-30 11:02:33 +0900

    [Passed] 정수 사각형 최소 합 (311ms, 21MB)

---
+++ 260730/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    static int ans = 0;
+    static int len;
+    static int m;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] memo;
+    static int[][] arr;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        memo = new int[101][101];
+        arr = new int[101][101];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        dp(n-1,0);
+        System.out.println(memo[n-1][0]);
+    }
+    static int dp(int x, int y) {
+        if(x == 0 && y == n - 1)
+            return arr[x][y];
+        if(memo[x][y] == 0) {
+            int up = 1000000000;
+            if(x > 0)
+                up = dp(x-1,y);
+            int right = 1000000000;
+            if(y < n - 1)
+                right = dp(x, y + 1);
+            memo[x][y] = Math.min(up, right);
+            memo[x][y] += arr[x][y];
+        }
+        return memo[x][y];
+    }
+    //
+    static boolean inRange(int x, int y, int lenX, int lenY) {
+        return x >= 0 && y >= 0 && x < lenX && y < lenY;
+    }
+    static class pair {
+        int x;
+        int y;
+        int cnt = 0;
+        int now = 0;
+        public pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public pair(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+}
+
+
