98dc1c55de9262656ff3a42fb3e797daeebb30e6
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-08-04 11:06:58 +0900

    [Passed] 정수 사각형 차이의 최소 2 (308ms, 21MB)

---
+++ 260804/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    static int ans = 1000;
+    static int len;
+    static int m;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] memo;
+    static int[][][] memo2;
+    static int[][] arr;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        memo2 = new int[n + 1][n + 1][2];
+
+        arr = new int[n + 1][n + 1];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int min = Math.min(arr[0][0], arr[n-1][n-1]);
+        for(int i = 1; i <= min; i++) {
+            memo = new int[n + 1][n + 1];
+            int max = dp(n - 1, n - 1, i);
+            int gap = max - i;
+            if(ans > gap && memo[0][0] != 0) {
+                ans = gap;
+            }
+        }
+
+        System.out.println(ans);
+    }
+    static int dp(int x, int y, int min) {
+        if(memo[x][y] == 0) {
+            int left = 1000;
+            int up = 1000;
+            if(x > 0) {
+                if(arr[x - 1][y] >= min) {
+                    up = dp(x-1, y, min);
+                }
+            }
+            if(y > 0) {
+                if(arr[x][y - 1] >= min) {
+                    left = dp(x, y - 1, min);
+                }
+            }
+            if(left + up == 2000) {
+                if(x == 0 && y == 0) {
+                    memo[x][y] = arr[x][y];
+                }
+                else {
+                    memo[x][y] = 1000;
+                }
+            }
+            else {
+                memo[x][y] = Math.max(arr[x][y], Math.min(left, up));
+            }
+        }
+        return memo[x][y];
+    }
+
+    //
+    static boolean inRange(int x, int y, int lenX, int lenY) {
+        return x >= 0 && y >= 0 && x < lenX && y < lenY;
+    }
+    static class pair {
+        int x;
+        int y;
+        int cnt = 0;
+        int now = 0;
+        int min;
+        int max;
+        public pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public pair(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+}
+
+
