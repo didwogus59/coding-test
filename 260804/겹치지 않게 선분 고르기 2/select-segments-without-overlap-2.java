b949e9a6087f8195563abea5a820bf9c4b8f679d
didwogus59 <48164086+didwogus59@users.noreply.github.com>
2026-08-04 17:49:40 +0900

    [Passed] 겹치지 않게 선분 고르기 2 (366ms, 17MB)

---
+++ 260804/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    static int ans = 0;
+    static int len;
+    static int m;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[] memo;
+    static int[][][] memo2;
+    static int[][] arr;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+//        m = sc.nextInt();
+        arr = new int[n + 1][2];
+        List<pair> list = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            list.add(new pair(x,y));
+        }
+        Collections.sort(list, (a, b) -> {
+            return a.x - b.x;
+        });
+        memo = new int[n + 1];
+        for(int i = 0; i < n; i++) {
+            arr[i][0] = list.get(i).x;
+            arr[i][1] = list.get(i).y;
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            ans = Math.max(ans, dp(i));
+        }
+        System.out.println(ans);
+    }
+    static int dp(int x) {
+        if(memo[x] == 0) {
+            int max = 0;
+            int now = arr[x][0];
+            for(int i = x - 1; i >= 0; i--) {
+                if(arr[i][1] < now) {
+                    max = Math.max(max, dp(i));
+                }
+            }
+            memo[x] = max + 1;
+        }
+        return memo[x];
+    }
+
+    //
+    static boolean inRange(int x, int y, int lenX, int lenY) {
+        return x >= 0 && y >= 0 && x < lenX && y < lenY;
+    }
+    static class pair {
+        int x;
+        int y;
+        int cnt = 0;
+        int now = 0;
+        int min;
+        int max;
+        public pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public pair(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+}
+
+
